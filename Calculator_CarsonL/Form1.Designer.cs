bf7bc5225756cc4c55c8095c3d176bc535e850f6
Carson1344 <carsonlanter427@gmail.com>
2026-09-22 14:53:20 -0500

    Initial commit

---
+++ Calculator_CarsonL/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Calculator_CarsonL
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button1.BackColor = Color.MediumPurple;
+            button1.BackgroundImageLayout = ImageLayout.Center;
+            button1.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.ImageAlign = ContentAlignment.TopCenter;
+            button1.Location = new Point(36, 201);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 54);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Orange;
+            label1.Location = new Point(12, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 34);
+            label1.TabIndex = 1;
+            label1.Text = "Calculator";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Font = new Font("Viner Hand ITC", 21.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(495, 97);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 47);
+            label2.TabIndex = 2;
+            label2.Text = "Result";
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.Maroon;
+            button11.Font = new Font("Viner Hand ITC", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button11.Location = new Point(524, 198);
+            button11.Name = "button11";
+            button11.Size = new Size(105, 42);
+            button11.TabIndex = 12;
+            button11.Text = "%";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.Maroon;
+            button12.Font = new Font("Viner Hand ITC", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button12.Location = new Point(524, 246);
+            button12.Name = "button12";
+            button12.Size = new Size(105, 42);
+            button12.TabIndex = 13;
+            button12.Text = "X";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.Maroon;
+            button13.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button13.Location = new Point(524, 294);
+            button13.Name = "button13";
+            button13.Size = new Size(105, 42);
+            button13.TabIndex = 14;
+            button13.Text = "-";
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.Maroon;
+            button15.Font = new Font("Viner Hand ITC", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button15.Location = new Point(524, 342);
+            button15.Name = "button15";
+            button15.Size = new Size(105, 42);
+            button15.TabIndex = 16;
+            button15.Text = "+";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += button15_Click;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.Maroon;
+            button16.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button16.Location = new Point(478, 414);
+            button16.Name = "button16";
+            button16.Size = new Size(151, 42);
+            button16.TabIndex = 17;
+            button16.Text = "=";
+            button16.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button2.BackColor = Color.MediumPurple;
+            button2.BackgroundImageLayout = ImageLayout.Center;
+            button2.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button2.ImageAlign = ContentAlignment.TopCenter;
+            button2.Location = new Point(192, 201);
+            button2.Name = "button2";
+            button2.Size = new Size(159, 54);
+            button2.TabIndex = 18;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button3.BackColor = Color.MediumPurple;
+            button3.BackgroundImageLayout = ImageLayout.Center;
+            button3.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button3.ImageAlign = ContentAlignment.TopCenter;
+            button3.Location = new Point(357, 201);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 54);
+            button3.TabIndex = 19;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button4.BackColor = Color.MediumPurple;
+            button4.BackgroundImageLayout = ImageLayout.Center;
+            button4.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button4.ImageAlign = ContentAlignment.TopCenter;
+            button4.Location = new Point(36, 273);
+            button4.Name = "button4";
+            button4.Size = new Size(150, 52);
+            button4.TabIndex = 20;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button5.BackColor = Color.MediumPurple;
+            button5.BackgroundImageLayout = ImageLayout.Center;
+            button5.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button5.ImageAlign = ContentAlignment.TopCenter;
+            button5.Location = new Point(192, 273);
+            button5.Name = "button5";
+            button5.Size = new Size(159, 52);
+            button5.TabIndex = 21;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button6.BackColor = Color.MediumPurple;
+            button6.BackgroundImageLayout = ImageLayout.Center;
+            button6.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button6.ImageAlign = ContentAlignment.TopCenter;
+            button6.Location = new Point(357, 273);
+            button6.Name = "button6";
+            button6.Size = new Size(150, 52);
+            button6.TabIndex = 22;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button7.BackColor = Color.MediumPurple;
+            button7.BackgroundImageLayout = ImageLayout.Center;
+            button7.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button7.ImageAlign = ContentAlignment.TopCenter;
+            button7.Location = new Point(201, 410);
+            button7.Name = "button7";
+            button7.Size = new Size(159, 52);
+            button7.TabIndex = 23;
+            button7.Text = "0";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button8.BackColor = Color.MediumPurple;
+            button8.BackgroundImageLayout = ImageLayout.Center;
+            button8.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button8.ImageAlign = ContentAlignment.TopCenter;
+            button8.Location = new Point(36, 342);
+            button8.Name = "button8";
+            button8.Size = new Size(150, 56);
+            button8.TabIndex = 24;
+            button8.Text = "7";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button9.BackColor = Color.MediumPurple;
+            button9.BackgroundImageLayout = ImageLayout.Center;
+            button9.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button9.ImageAlign = ContentAlignment.TopCenter;
+            button9.Location = new Point(201, 342);
+            button9.Name = "button9";
+            button9.Size = new Size(150, 56);
+            button9.TabIndex = 25;
+            button9.Text = "8";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button10.BackColor = Color.MediumPurple;
+            button10.BackgroundImageLayout = ImageLayout.Center;
+            button10.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button10.ImageAlign = ContentAlignment.TopCenter;
+            button10.Location = new Point(357, 342);
+            button10.Name = "button10";
+            button10.Size = new Size(150, 56);
+            button10.TabIndex = 26;
+            button10.Text = "9";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.BackColor = SystemColors.MenuHighlight;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(150, 14);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(124, 181);
+            pictureBox1.TabIndex = 27;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.MenuHighlight;
+            ClientSize = new Size(641, 468);
+            Controls.Add(pictureBox1);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Cursor = Cursors.Hand;
+            ForeColor = Color.Orange;
+            Name = "Form1";
+            Text = "My App";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button15;
+        private Button button16;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Calculator_CarsonL/Form1.cs
@@ -0,0 +1,35 @@
+namespace Calculator_CarsonL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
